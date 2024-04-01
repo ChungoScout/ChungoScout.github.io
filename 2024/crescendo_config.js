060778dfd6ca4d0c1f4eb0b9ba7ff63d0d155560
ChungoScout <165527445+ChungoScout@users.noreply.github.com>
2024-03-31 22:26:33 -0400

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024OHCL",
+      "defaultValue": "2024OHMV",
       "required": "true"
     },
     { "name": "Match Level",
